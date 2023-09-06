0245bc0eec1eed9b77bc5111c8020293345dfe2c
Aashisha Negi <500091664@stu.upes.ac.in>
2023-09-06 11:18:10 +0530

    Create RegistrationLoginSpringBootSecurityThymeleafApplication.java

---
+++ src/main/java/RegistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -0,0 +1,18 @@
+package com.reg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+@SpringBootApplication
+
+
+public class RegistrationLoginSpringBootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringBootSecurityThymeleafApplication.class, args);
+	}
+
+}
