d214603611ffc4ec6341dfd3d02bd6cd2a2927fa
Aashisha Negi <500091664@stu.upes.ac.in>
2023-09-06 11:26:03 +0530

    Create RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java

---
+++ src/test/java/RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reg.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringBootSecurityThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
